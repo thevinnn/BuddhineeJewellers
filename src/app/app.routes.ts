c99876df455e1ef065598145dc7422b101c8dba3
Thevin <thevindev@gmail.com>
2025-12-09 13:41:13 +1100

    setup homepage elements

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {Home} from './home/home';
+import {Homepage} from './homepage/homepage';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: Homepage},
+];
--- src/app/homepage/homepage.ts
+++ src/app/homepage/homepage.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {Home} from '../home/home';
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [Home],
   templateUrl: './homepage.html',
   styleUrl: './homepage.css',
 })
